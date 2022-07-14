dc695488ecbb5855fbef8e336561a3c8366ceef8
Jason Grant <jason.grant@umail.ucc.ie>
2022-07-14 10:16:40 +0100

    Bug Fix - make action icons on movie card configurable.

--- src/components/templateMovieListPage/index.js
+++ src/components/templateMovieListPage/index.js
@@ -19,7 +19,7 @@ const useStyles = makeStyles((theme) =>  ({
     right: theme.spacing(2),
   },
 }));
-function MovieListPageTemplate({ movies, title, selectFavourite }) {
+function MovieListPageTemplate({ movies, title, action }) {
     const classes = useStyles();
     const [titleFilter, setTitleFilter] = useState("");
     const [genreFilter, setGenreFilter] = useState("0");
@@ -47,7 +47,7 @@ function MovieListPageTemplate({ movies, title, selectFavourite }) {
           <Header title={title} />
         </Grid>
         <Grid item container spacing={5}>
-          <MovieList selectFavourite={selectFavourite} movies={displayedMovies} />
+          <MovieList action={action} movies={displayedMovies} />
         </Grid>
       </Grid>
       <Fab
